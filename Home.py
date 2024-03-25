9ad32e948b2d04385b7ce9211b09ef07f9a93b3d
jaonineX <kophakhin155@gmail.com>
2024-03-25 14:24:08 +0700

    ฟ

--- Home.py
+++ Home.py
@@ -6,7 +6,6 @@ import streamlit as st
 import seaborn as sns
 import pandas as pd
 
-
 gender_data = pd.read_csv('./data/shopping_trends.csv')
 
 html_example_table = """
@@ -24,26 +23,25 @@ html_count_by_age = """
 """
 st.markdown(html_count_by_age, unsafe_allow_html=True)
 
-
 plt.figure(figsize=(15, 5))
+sns.countplot(x='Gender', data=gender_data)  # แก้เป็น gender_data แทน shopping_trends.csv
 plt.title('จำนวนเพศตามช่วงอายุ')
-plt.xlabel('ช่วงอายุ')
+plt.xlabel('เพศ')
 plt.ylabel('จำนวน')
 plt.xticks(rotation=45)
 st.pyplot()
 
-html_sales_by_region = """
+html_sales_by_gender = """
 <div style="background-color:#0E1117;border-bottom: 3px solid #ffffff;border-top: 3px solid #ffffff;">
 <center><h3>ยอดขายตามภูมิภาค</h3></center>
 </div>
 """
-st.markdown(html_sales_by_region, unsafe_allow_html=True)
+st.markdown(html_sales_by_gender, unsafe_allow_html=True)
 
 plt.figure(figsize=(15, 5))
-import seaborn as sns
-sns.countplot(x='Gender', data=shopping_trends.csv)
+sns.countplot(x='Gender', data=gender_data)  # แก้เป็น gender_data แทน shopping_trends.csv
 plt.title('ยอดขายตามภูมิภาค')
-plt.xlabel('ช่วงอายุ')
+plt.xlabel('ภูมิภาค')
 plt.ylabel('จำนวน')
 plt.xticks(rotation=45)
 st.pyplot()
