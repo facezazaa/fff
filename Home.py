a8573f10f2eb53a9c90e20e4fd02b17e571e1c65
jaonineX <kophakhin155@gmail.com>
2024-03-25 14:14:51 +0700

    a

--- Home.py
+++ Home.py
@@ -40,7 +40,7 @@ html_sales_by_region = """
 st.markdown(html_sales_by_region, unsafe_allow_html=True)
 
 plt.figure(figsize=(15, 5))
-sns.countplot(x='Male', data=shopping_trends)
+sns.countplot(x='Gender', data=shopping_trends)
 plt.title('ยอดขายตามภูมิภาค')
 plt.xlabel('ช่วงอายุ')
 plt.ylabel('จำนวน')
